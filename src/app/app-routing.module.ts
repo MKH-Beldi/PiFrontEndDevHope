ff27e4b1122befc837b157d3100f1927eb2f4d57
sahar <sahar.dhaya@esprit.tn>
2021-07-25 22:26:08 +0200

    after merge 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 import {ConsultationFormComponent} from './component/form/consultation-form/consultation-form.component';
 import {ConsultationTableComponent} from './component/table/consultation-table/consultation-table.component';
 import {SymptomTableComponent} from './component/table/symptom-table/symptom-table.component';
-import {SpecialtyDrListComponent} from './component/specialty-dr-list/specialty-dr-list.component';
 import {SymptomListComponent} from './component/symptom-list/symptom-list.component';
 import {SymptomFormComponent} from './component/form/symptom-form/symptom-form.component';
 
@@ -15,7 +14,6 @@ const routes: Routes = [
   { path: 'symptom/table', component: SymptomTableComponent },
   { path: 'symptom/list', component: SymptomListComponent },
   { path: 'symptom/add', component: SymptomFormComponent },
-  { path: 'test', component: SpecialtyDrListComponent },
 ];
 
 @NgModule({
