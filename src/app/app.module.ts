4a63468cf182834f49b0f09cb444046dcd3ee79a
sahar <sahar.dhaya@esprit.tn>
2021-07-25 20:23:05 +0200

    move schedule component under component folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,17 @@ import {LOCALE_ID, NgModule} from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserListComponent } from './user-list/user-list.component';
-import { SpecialtyDrListComponent } from './specialty-dr-list/specialty-dr-list.component';
-import { FormsModule } from '@angular/forms';
+import {ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import localeFr from '@angular/common/locales/fr';
 import {registerLocaleData} from '@angular/common';
-import { SpecialtyDrFormComponent } from './form/specialty-dr-form/specialty-dr-form.component';
-import {SpecialtyDrService} from './shared/specialty-dr.service';
 registerLocaleData(localeFr);
-import { ScheduleModule } from '@syncfusion/ej2-angular-schedule';
-
-import { DayService, WeekService, WorkWeekService, MonthService, AgendaService, MonthAgendaService,TimelineViewsService} from '@syncfusion/ej2-angular-schedule';
-import { ButtonModule} from "@syncfusion/ej2-angular-buttons";
-
-import { ScheduleComponent } from './schedule/schedule.component';
-import {DayPilot, DayPilotCalendarComponent} from "daypilot-pro-angular";
 import { FullCalendarModule } from '@fullcalendar/angular';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import {ScheduleComponent} from "./component/schedule/schedule.component";
+
 
 FullCalendarModule.registerPlugins([ // register FullCalendar plugins
   dayGridPlugin,
@@ -34,10 +25,8 @@ FullCalendarModule.registerPlugins([ // register FullCalendar plugins
 @NgModule({
   declarations: [
     AppComponent,
-    UserListComponent,
-    SpecialtyDrListComponent,
-    SpecialtyDrFormComponent,
     ScheduleComponent,
+
 
 
 
@@ -46,13 +35,13 @@ FullCalendarModule.registerPlugins([ // register FullCalendar plugins
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule,
-    ScheduleModule,
-    FullCalendarModule
-  ],
-  exports: [ScheduleComponent],
+    ReactiveFormsModule,
+    FullCalendarModule,
 
-  providers: [{ provide: LOCALE_ID, useValue: 'fr-FR'}, SpecialtyDrService, DayService, WeekService, WorkWeekService, MonthService, AgendaService, TimelineViewsService],
+  ],
+  exports: [ ScheduleComponent],
+
+  providers: [{ provide: LOCALE_ID, useValue: 'fr-FR'} ],
   bootstrap: [AppComponent]
 
 })
---
+++ src/app/component/schedule/schedule.component.ts
@@ -0,0 +1,128 @@
+
+import {Component, OnInit, ViewChild} from '@angular/core';
+import { CalendarOptions } from '@fullcalendar/angular';
+import {FormGroup, Validators} from '@angular/forms';
+import {ScheduleService} from "../../shared/schedule.service";
+import {Schedule} from "../../model/schedule";
+declare let $: any;
+
+@Component({
+  selector: 'app-schedule',
+  styleUrls: ['./schedule.component.css'],
+  providers: [],
+templateUrl: './schedule.component.html'
+
+})
+
+export class ScheduleComponent {
+
+  schedules: Schedule[];
+  s: Schedule;
+  list: [];
+  /* Add Event Form */
+  eventdate: string;
+  successdata: any;
+  addEventForm: FormGroup;
+  submitted = false;
+
+  calendarOptions: CalendarOptions = {
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+    },
+    initialView: 'timeGridWeek',
+    weekends: true,
+    editable: true,
+    selectable: true,
+    selectMirror: true,
+    dayMaxEvents: true,
+    dateClick: this.handleDateClick.bind(this), // bind is important!
+    events: []
+  };
+
+  constructor(private scheduleService: ScheduleService) {
+  }
+
+  ngOnInit(): void {
+
+    this.scheduleService.getSchedule().subscribe(
+      (data: any[]) => {
+        this.schedules = data;
+        this.calendarOptions.events = data;
+      }
+    );
+  }
+
+
+  handleDateClick(arg) {
+    console.log(typeof $);
+    $('#myModal').modal("show");
+    $(".modal-title").text("");
+    $(".modal-title").text("Add Event at : "+arg.dateStr);
+
+  }
+
+  onDateClick(res) {
+    alert('Clicked on date : ' + res.dateStr);
+  }
+
+  //handleDateClick(info) {
+  //alert('Clicked on: ' + info.dateStr);
+  //alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
+  ///alert('Current view: ' + info.view.type);
+  //alert('date click! ' + info.dateStr);
+  // info.dayEl.style.backgroundColor = 'red';
+  //  const title = prompt('Please enter a new title for your event');
+  //  const calendarApi = info.view.calendar;
+
+  //calendarApi.unselect(); // clear date selection
+  // }
+
+
+  //Add user form actions
+  get f() {
+    return this.addEventForm.controls;
+  }
+
+  onSubmit() {
+
+    this.submitted = true;
+    // stop here if form is invalid and reset the validations
+    this.addEventForm.get('title').setValidators([Validators.required]);
+    this.addEventForm.get('title').updateValueAndValidity();
+    console.warn(this.addEventForm.value);
+
+    if (this.addEventForm.invalid) {
+      return;
+    }
+    //Form Submittion and send data via API
+    if (this.submitted) {
+      // Initialize Params Object
+      var myFormData = new FormData();
+      // Begin assigning parameters
+      myFormData.append('title', this.addEventForm.value.title);
+      myFormData.append('start', this.eventdate);
+      myFormData.append('end', this.eventdate);
+      return this.scheduleService.addSchedule(this.s).subscribe();
+    }
+  }
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
